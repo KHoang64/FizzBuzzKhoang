751fafa8ba72b7b70129e01051556d7a9c5747b3
Kent ong <hkent625@gmail.com>
2025-09-10 21:05:34 -0400

    modifcation: modulo as bool

--- src/FizzBuzz.cpp
+++ src/FizzBuzz.cpp
@@ -2,50 +2,71 @@
 
 #include <iostream>
 
+
 /*
-    DIRECTIONS:
-
-    FizzBuzz is a common coding interview challenge that requires the programmer
-
-
-    -- to print numbers from 1 through n, --
-
-     but with specific replacements.
-
-
-    If the number is divisible by 3, print "Fizz". - check
-
-    If the number is divisible by
-
-    5, print "Buzz".
-
-     If the number is divisible by both 3 and 5, print "FizzBuzz"
-
-    Each number or string should be followed by a newline character.
-
-    Program FizzBuzz with the given int variable n.
+@param n: the number to loop to (inclusive)
+Prints Fizz if divisible by 3
+Prints Buzz if divisible by 5
+Prints FizzBuzz if both
+else just print current number in [1,n]
 */
 
 void FizzBuzz(int n)
 {
-    for (int i = 1; i <= n; i++)
-    {
 
-        if (i % 5 == 0 && i % 3 == 0)
-        {
+
+
+#pragma region Old code v1
+
+// for (int i =0; i<= n ; i++){
+//     if ( (i % 3 == 0 ) && (i % 5 ==0  ) ){
+//         std::cout<<"FizzBuzz\n";
+
+//     }
+//     else if ( (i % 5 == 0 ) ){
+//         std::cout<<"Buzz\n";
+//     }
+//     else if ((i% 3 == 0)){
+//         std::cout<<"Fizz\n";
+//     }
+//     else{
+//         std::cout << i <<"\n";
+//     }
+#pragma endregion
+
+#pragma region Notes for v2
+// demo of mod as bool
+
+//      if (i % 4) 
+//      {
+//         //note: Runs if i is NOT divisible by 4
+//      }
+
+//Same behavior
+//      if ((i % 4) != 0) {
+//          
+//      }
+    
+#pragma endregion
+
+#pragma region Code v2 no == 0
+//________________________________
+
+    for (int i = 1; i <= n; i++) {
+        if (!(i % 3) && !(i % 5)) {
             std::cout << "FizzBuzz\n";
         }
-        else if (i % 5 == 0)
-        {
+        else if (!(i % 5)) {
             std::cout << "Buzz\n";
         }
-        else if (i % 3 == 0)
-        {
+        else if (!(i % 3)) {
             std::cout << "Fizz\n";
         }
-        else
-        {
+        else {
             std::cout << i << "\n";
         }
     }
+#pragma endregion
+
+
 }
